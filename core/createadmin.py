d5d884894690126d616635b7c0cb132a63c707b9
issambd29 <badaouiissam660@gmail.com>
2026-04-19 20:12:51 +0100

    add render config

---
+++ core/createadmin.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@example.com',
+                password='admin123'
+            )

49e3f515ccaa31056c60f1c2fdd83b6295a04488
issambd29 <badaouiissam660@gmail.com>
2026-04-19 19:31:36 +0100

    auto migrate

--- school_project/wsgi.py
+++ school_project/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_project.settings')
 
 application = get_wsgi_application()
+
+# 🔥 هذا السطر يضيف migrate تلقائي
+from django.core.management import call_command
+call_command('migrate')
